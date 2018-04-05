0280514e32f12b9b7583fcb4b44007bdbc9a357f
lawyerh <lawyerh_01@yahoo.com>
2018-04-05 10:48:13 -0700

    asowmer

--- Controllers/CardsController.cs
+++ Controllers/CardsController.cs
@@ -20,21 +20,30 @@ namespace thirty_one
         [Route("players")]
         public IActionResult CreatePlayers(string player1, string player2, string player3, string player4) // Post route to process player submission. Instantiates all players and A.I.
         {
+            HttpContext.Session.SetInt32("NumPlayers", 0);
             if(player1 != null)
             {
                 HttpContext.Session.SetString("player1", player1);
+                int? NumPlayers = HttpContext.Session.GetInt32("NumPlayers");
+                HttpContext.Session.SetInt32("NumPlayers", (int)NumPlayers+1);
             }
             if(player2 != null)
             {
                 HttpContext.Session.SetString("player2", player2);
+                int? NumPlayers = HttpContext.Session.GetInt32("NumPlayers");
+                HttpContext.Session.SetInt32("NumPlayers", (int)NumPlayers+1);
             }
             if(player3 != null)
             {
                 HttpContext.Session.SetString("player3", player3);
+                int? NumPlayers = HttpContext.Session.GetInt32("NumPlayers");
+                HttpContext.Session.SetInt32("NumPlayers", (int)NumPlayers+1);
             }
             if(player4 != null)
             {
                 HttpContext.Session.SetString("player4", player4);
+                int? NumPlayers = HttpContext.Session.GetInt32("NumPlayers");
+                HttpContext.Session.SetInt32("NumPlayers", (int)NumPlayers+1);
             }
             return RedirectToAction("Main");
         }
@@ -43,6 +52,8 @@ namespace thirty_one
         [Route("gameboard")]
         public IActionResult Main()
         {
+            int? NumPlayers = HttpContext.Session.GetInt32("NumPlayers");
+            ViewBag.NumPlayers = NumPlayers;
             return View();
         }
     }
