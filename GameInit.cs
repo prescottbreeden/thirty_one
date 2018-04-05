5c1d156619dda8f731a7bfa70df35c58ee9fedc4
Prescott Breeden <prescott.breeden@gmail.com>
2018-04-04 18:13:33 -0700

    initial classes created

---
+++ GameInit.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace thirty_one
+{
+    public class GameInit
+    {
+        public List<Player> Players = new List<Player>();
+        public int TokenPot;
+        
+        public GameInit()
+        {
+            
+        }
+    }
+}
